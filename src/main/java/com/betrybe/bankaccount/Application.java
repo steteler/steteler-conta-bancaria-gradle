f41c6d57cc28d44b51d92d38d82dbc64a29d6f61
steteler <joaovictorkikuti@gmail.com>
2023-11-26 20:07:15 -0300

    feat: terminado 3 requisito

--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -1,5 +1,8 @@
 package com.betrybe.bankaccount;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * A class.
  */
@@ -9,7 +12,18 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    AccountNumberFormatter account = new AccountNumberFormatter();
+    Scanner menuScanner = new Scanner(System.in);
 
+    try {
+      System.out.println("Informe o número da conta:");
+      int literalAccountNumber = menuScanner.nextInt();
+
+      System.out.println(
+          "Número da conta: " + account.formatAccountNumber(literalAccountNumber)
+      );
+    } catch (InputMismatchException e) {
+      System.out.println("Número da conta inválido!");
+    }
   }
-
 }
